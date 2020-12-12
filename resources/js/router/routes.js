bc44ba2fb4859a8bf26876f236448d387ad512cc
AlanConstantino <constantinoalan98@gmail.com>
2020-12-12 10:59:19 -0800

    Added vue routes

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -13,6 +13,9 @@ export default [
   { path: '/email/resend', name: 'verification.resend', component: page('auth/verification/resend.vue') },
 
   { path: '/home', name: 'home', component: page('home.vue') },
+  { path: '/add', name: 'add', component: page('addCar.vue') },
+  { path: '/edit/:id', name: 'edit', component: page('editCar.vue') },
+
   {
     path: '/settings',
     component: page('settings/index.vue'),
